dc70a53659b9a280cba8e995a298a4dfdd4a2f95
helcl42 <lubos.helcl@gmail.com>
2025-12-16 23:28:39 +0100

    Fix win32 dll mode

--- gfx/GfxApi.h
+++ gfx/GfxApi.h
@@ -4,6 +4,35 @@
 #include <stddef.h>
 #include <stdint.h>
 
+// ============================================================================
+// DLL Export/Import Macros for Windows
+// ============================================================================
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+    #ifdef GFX_BUILDING_DLL
+        // Building the DLL - export symbols
+        #ifdef __GNUC__
+            #define GFX_API __attribute__((dllexport))
+        #else
+            #define GFX_API __declspec(dllexport)
+        #endif
+    #else
+        // Using the DLL - import symbols
+        #ifdef __GNUC__
+            #define GFX_API __attribute__((dllimport))
+        #else
+            #define GFX_API __declspec(dllimport)
+        #endif
+    #endif
+#else
+    // Non-Windows platforms - use visibility attributes for shared libraries
+    #if defined(__GNUC__) && __GNUC__ >= 4
+        #define GFX_API __attribute__((visibility("default")))
+    #else
+        #define GFX_API
+    #endif
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -551,159 +580,159 @@ typedef struct {
 // These should be called before creating any instances
 // Call gfxLoadBackend or gfxLoadAllBackends at application startup
 // Call gfxUnloadBackend or gfxUnloadAllBackends at application shutdown
-bool gfxLoadBackend(GfxBackend backend);
-void gfxUnloadBackend(GfxBackend backend);
-bool gfxLoadAllBackends(void);
-void gfxUnloadAllBackends(void);
+GFX_API bool gfxLoadBackend(GfxBackend backend);
+GFX_API void gfxUnloadBackend(GfxBackend backend);
+GFX_API bool gfxLoadAllBackends(void);
+GFX_API void gfxUnloadAllBackends(void);
 
 // Instance functions
-GfxResult gfxCreateInstance(const GfxInstanceDescriptor* descriptor, GfxInstance* outInstance);
-void gfxInstanceDestroy(GfxInstance instance);
-GfxResult gfxInstanceRequestAdapter(GfxInstance instance, const GfxAdapterDescriptor* descriptor, GfxAdapter* outAdapter);
-uint32_t gfxInstanceEnumerateAdapters(GfxInstance instance, GfxAdapter* adapters, uint32_t maxAdapters);
+GFX_API GfxResult gfxCreateInstance(const GfxInstanceDescriptor* descriptor, GfxInstance* outInstance);
+GFX_API void gfxInstanceDestroy(GfxInstance instance);
+GFX_API GfxResult gfxInstanceRequestAdapter(GfxInstance instance, const GfxAdapterDescriptor* descriptor, GfxAdapter* outAdapter);
+GFX_API uint32_t gfxInstanceEnumerateAdapters(GfxInstance instance, GfxAdapter* adapters, uint32_t maxAdapters);
 
 // Adapter functions
-void gfxAdapterDestroy(GfxAdapter adapter);
-GfxResult gfxAdapterCreateDevice(GfxAdapter adapter, const GfxDeviceDescriptor* descriptor, GfxDevice* outDevice);
-const char* gfxAdapterGetName(GfxAdapter adapter);
-GfxBackend gfxAdapterGetBackend(GfxAdapter adapter);
+GFX_API void gfxAdapterDestroy(GfxAdapter adapter);
+GFX_API GfxResult gfxAdapterCreateDevice(GfxAdapter adapter, const GfxDeviceDescriptor* descriptor, GfxDevice* outDevice);
+GFX_API const char* gfxAdapterGetName(GfxAdapter adapter);
+GFX_API GfxBackend gfxAdapterGetBackend(GfxAdapter adapter);
 
 // Device functions
-void gfxDeviceDestroy(GfxDevice device);
-GfxQueue gfxDeviceGetQueue(GfxDevice device);
-GfxResult gfxDeviceCreateSurface(GfxDevice device, const GfxSurfaceDescriptor* descriptor, GfxSurface* outSurface);
-GfxResult gfxDeviceCreateSwapchain(GfxDevice device, GfxSurface surface, const GfxSwapchainDescriptor* descriptor, GfxSwapchain* outSwapchain);
-GfxResult gfxDeviceCreateBuffer(GfxDevice device, const GfxBufferDescriptor* descriptor, GfxBuffer* outBuffer);
-GfxResult gfxDeviceCreateTexture(GfxDevice device, const GfxTextureDescriptor* descriptor, GfxTexture* outTexture);
-GfxResult gfxDeviceCreateSampler(GfxDevice device, const GfxSamplerDescriptor* descriptor, GfxSampler* outSampler);
-GfxResult gfxDeviceCreateShader(GfxDevice device, const GfxShaderDescriptor* descriptor, GfxShader* outShader);
-GfxResult gfxDeviceCreateBindGroupLayout(GfxDevice device, const GfxBindGroupLayoutDescriptor* descriptor, GfxBindGroupLayout* outLayout);
-GfxResult gfxDeviceCreateBindGroup(GfxDevice device, const GfxBindGroupDescriptor* descriptor, GfxBindGroup* outBindGroup);
-GfxResult gfxDeviceCreateRenderPipeline(GfxDevice device, const GfxRenderPipelineDescriptor* descriptor, GfxRenderPipeline* outPipeline);
-GfxResult gfxDeviceCreateComputePipeline(GfxDevice device, const GfxComputePipelineDescriptor* descriptor, GfxComputePipeline* outPipeline);
-GfxResult gfxDeviceCreateCommandEncoder(GfxDevice device, const char* label, GfxCommandEncoder* outEncoder);
-GfxResult gfxDeviceCreateFence(GfxDevice device, const GfxFenceDescriptor* descriptor, GfxFence* outFence);
-GfxResult gfxDeviceCreateSemaphore(GfxDevice device, const GfxSemaphoreDescriptor* descriptor, GfxSemaphore* outSemaphore);
-void gfxDeviceWaitIdle(GfxDevice device);
+GFX_API void gfxDeviceDestroy(GfxDevice device);
+GFX_API GfxQueue gfxDeviceGetQueue(GfxDevice device);
+GFX_API GfxResult gfxDeviceCreateSurface(GfxDevice device, const GfxSurfaceDescriptor* descriptor, GfxSurface* outSurface);
+GFX_API GfxResult gfxDeviceCreateSwapchain(GfxDevice device, GfxSurface surface, const GfxSwapchainDescriptor* descriptor, GfxSwapchain* outSwapchain);
+GFX_API GfxResult gfxDeviceCreateBuffer(GfxDevice device, const GfxBufferDescriptor* descriptor, GfxBuffer* outBuffer);
+GFX_API GfxResult gfxDeviceCreateTexture(GfxDevice device, const GfxTextureDescriptor* descriptor, GfxTexture* outTexture);
+GFX_API GfxResult gfxDeviceCreateSampler(GfxDevice device, const GfxSamplerDescriptor* descriptor, GfxSampler* outSampler);
+GFX_API GfxResult gfxDeviceCreateShader(GfxDevice device, const GfxShaderDescriptor* descriptor, GfxShader* outShader);
+GFX_API GfxResult gfxDeviceCreateBindGroupLayout(GfxDevice device, const GfxBindGroupLayoutDescriptor* descriptor, GfxBindGroupLayout* outLayout);
+GFX_API GfxResult gfxDeviceCreateBindGroup(GfxDevice device, const GfxBindGroupDescriptor* descriptor, GfxBindGroup* outBindGroup);
+GFX_API GfxResult gfxDeviceCreateRenderPipeline(GfxDevice device, const GfxRenderPipelineDescriptor* descriptor, GfxRenderPipeline* outPipeline);
+GFX_API GfxResult gfxDeviceCreateComputePipeline(GfxDevice device, const GfxComputePipelineDescriptor* descriptor, GfxComputePipeline* outPipeline);
+GFX_API GfxResult gfxDeviceCreateCommandEncoder(GfxDevice device, const char* label, GfxCommandEncoder* outEncoder);
+GFX_API GfxResult gfxDeviceCreateFence(GfxDevice device, const GfxFenceDescriptor* descriptor, GfxFence* outFence);
+GFX_API GfxResult gfxDeviceCreateSemaphore(GfxDevice device, const GfxSemaphoreDescriptor* descriptor, GfxSemaphore* outSemaphore);
+GFX_API void gfxDeviceWaitIdle(GfxDevice device);
 
 // Surface functions
-void gfxSurfaceDestroy(GfxSurface surface);
-uint32_t gfxSurfaceGetWidth(GfxSurface surface);
-uint32_t gfxSurfaceGetHeight(GfxSurface surface);
-void gfxSurfaceResize(GfxSurface surface, uint32_t width, uint32_t height);
-uint32_t gfxSurfaceGetSupportedFormats(GfxSurface surface, GfxTextureFormat* formats, uint32_t maxFormats);
-uint32_t gfxSurfaceGetSupportedPresentModes(GfxSurface surface, GfxPresentMode* presentModes, uint32_t maxModes);
-GfxPlatformWindowHandle gfxSurfaceGetPlatformHandle(GfxSurface surface);
+GFX_API void gfxSurfaceDestroy(GfxSurface surface);
+GFX_API uint32_t gfxSurfaceGetWidth(GfxSurface surface);
+GFX_API uint32_t gfxSurfaceGetHeight(GfxSurface surface);
+GFX_API void gfxSurfaceResize(GfxSurface surface, uint32_t width, uint32_t height);
+GFX_API uint32_t gfxSurfaceGetSupportedFormats(GfxSurface surface, GfxTextureFormat* formats, uint32_t maxFormats);
+GFX_API uint32_t gfxSurfaceGetSupportedPresentModes(GfxSurface surface, GfxPresentMode* presentModes, uint32_t maxModes);
+GFX_API GfxPlatformWindowHandle gfxSurfaceGetPlatformHandle(GfxSurface surface);
 
 // Swapchain functions
-void gfxSwapchainDestroy(GfxSwapchain swapchain);
-uint32_t gfxSwapchainGetWidth(GfxSwapchain swapchain);
-uint32_t gfxSwapchainGetHeight(GfxSwapchain swapchain);
-GfxTextureFormat gfxSwapchainGetFormat(GfxSwapchain swapchain);
-uint32_t gfxSwapchainGetBufferCount(GfxSwapchain swapchain);
-GfxTextureView gfxSwapchainGetCurrentTextureView(GfxSwapchain swapchain);
-GfxResult gfxSwapchainPresent(GfxSwapchain swapchain);
-void gfxSwapchainResize(GfxSwapchain swapchain, uint32_t width, uint32_t height);
-bool gfxSwapchainNeedsRecreation(GfxSwapchain swapchain);
+GFX_API void gfxSwapchainDestroy(GfxSwapchain swapchain);
+GFX_API uint32_t gfxSwapchainGetWidth(GfxSwapchain swapchain);
+GFX_API uint32_t gfxSwapchainGetHeight(GfxSwapchain swapchain);
+GFX_API GfxTextureFormat gfxSwapchainGetFormat(GfxSwapchain swapchain);
+GFX_API uint32_t gfxSwapchainGetBufferCount(GfxSwapchain swapchain);
+GFX_API GfxTextureView gfxSwapchainGetCurrentTextureView(GfxSwapchain swapchain);
+GFX_API GfxResult gfxSwapchainPresent(GfxSwapchain swapchain);
+GFX_API void gfxSwapchainResize(GfxSwapchain swapchain, uint32_t width, uint32_t height);
+GFX_API bool gfxSwapchainNeedsRecreation(GfxSwapchain swapchain);
 
 // Buffer functions
-void gfxBufferDestroy(GfxBuffer buffer);
-uint64_t gfxBufferGetSize(GfxBuffer buffer);
-GfxBufferUsage gfxBufferGetUsage(GfxBuffer buffer);
-GfxResult gfxBufferMapAsync(GfxBuffer buffer, uint64_t offset, uint64_t size, void** outMappedPointer);
-void gfxBufferUnmap(GfxBuffer buffer);
+GFX_API void gfxBufferDestroy(GfxBuffer buffer);
+GFX_API uint64_t gfxBufferGetSize(GfxBuffer buffer);
+GFX_API GfxBufferUsage gfxBufferGetUsage(GfxBuffer buffer);
+GFX_API GfxResult gfxBufferMapAsync(GfxBuffer buffer, uint64_t offset, uint64_t size, void** outMappedPointer);
+GFX_API void gfxBufferUnmap(GfxBuffer buffer);
 
 // Texture functions
-void gfxTextureDestroy(GfxTexture texture);
-GfxExtent3D gfxTextureGetSize(GfxTexture texture);
-GfxTextureFormat gfxTextureGetFormat(GfxTexture texture);
-uint32_t gfxTextureGetMipLevelCount(GfxTexture texture);
-uint32_t gfxTextureGetSampleCount(GfxTexture texture);
-GfxTextureUsage gfxTextureGetUsage(GfxTexture texture);
-GfxResult gfxTextureCreateView(GfxTexture texture, const GfxTextureViewDescriptor* descriptor, GfxTextureView* outView);
+GFX_API void gfxTextureDestroy(GfxTexture texture);
+GFX_API GfxExtent3D gfxTextureGetSize(GfxTexture texture);
+GFX_API GfxTextureFormat gfxTextureGetFormat(GfxTexture texture);
+GFX_API uint32_t gfxTextureGetMipLevelCount(GfxTexture texture);
+GFX_API uint32_t gfxTextureGetSampleCount(GfxTexture texture);
+GFX_API GfxTextureUsage gfxTextureGetUsage(GfxTexture texture);
+GFX_API GfxResult gfxTextureCreateView(GfxTexture texture, const GfxTextureViewDescriptor* descriptor, GfxTextureView* outView);
 
 // TextureView functions
-void gfxTextureViewDestroy(GfxTextureView textureView);
-GfxTexture gfxTextureViewGetTexture(GfxTextureView textureView);
+GFX_API void gfxTextureViewDestroy(GfxTextureView textureView);
+GFX_API GfxTexture gfxTextureViewGetTexture(GfxTextureView textureView);
 
 // Sampler functions
-void gfxSamplerDestroy(GfxSampler sampler);
+GFX_API void gfxSamplerDestroy(GfxSampler sampler);
 
 // Shader functions
-void gfxShaderDestroy(GfxShader shader);
+GFX_API void gfxShaderDestroy(GfxShader shader);
 
 // BindGroupLayout functions
-void gfxBindGroupLayoutDestroy(GfxBindGroupLayout bindGroupLayout);
+GFX_API void gfxBindGroupLayoutDestroy(GfxBindGroupLayout bindGroupLayout);
 
 // BindGroup functions
-void gfxBindGroupDestroy(GfxBindGroup);
+GFX_API void gfxBindGroupDestroy(GfxBindGroup);
 
 // RenderPipeline functions
-void gfxRenderPipelineDestroy(GfxRenderPipeline renderPipeline);
+GFX_API void gfxRenderPipelineDestroy(GfxRenderPipeline renderPipeline);
 
 // ComputePipeline functions
-void gfxComputePipelineDestroy(GfxComputePipeline computePipeline);
+GFX_API void gfxComputePipelineDestroy(GfxComputePipeline computePipeline);
 
 // Queue functions
-GfxResult gfxQueueSubmit(GfxQueue queue, GfxCommandEncoder commandEncoder);
-GfxResult gfxQueueSubmitWithSync(GfxQueue queue, const GfxSubmitInfo* submitInfo);
-void gfxQueueWriteBuffer(GfxQueue queue, GfxBuffer buffer, uint64_t offset, const void* data, uint64_t size);
-void gfxQueueWriteTexture(GfxQueue queue, GfxTexture texture, const GfxOrigin3D* origin, uint32_t mipLevel,
+GFX_API GfxResult gfxQueueSubmit(GfxQueue queue, GfxCommandEncoder commandEncoder);
+GFX_API GfxResult gfxQueueSubmitWithSync(GfxQueue queue, const GfxSubmitInfo* submitInfo);
+GFX_API void gfxQueueWriteBuffer(GfxQueue queue, GfxBuffer buffer, uint64_t offset, const void* data, uint64_t size);
+GFX_API void gfxQueueWriteTexture(GfxQueue queue, GfxTexture texture, const GfxOrigin3D* origin, uint32_t mipLevel,
     const void* data, uint64_t dataSize, uint32_t bytesPerRow, const GfxExtent3D* extent);
-GfxResult gfxQueueWaitIdle(GfxQueue queue);
+GFX_API GfxResult gfxQueueWaitIdle(GfxQueue queue);
 
 // CommandEncoder functions
-void gfxCommandEncoderDestroy(GfxCommandEncoder commandEncoder);
-GfxResult gfxCommandEncoderBeginRenderPass(GfxCommandEncoder commandEncoder,
+GFX_API void gfxCommandEncoderDestroy(GfxCommandEncoder commandEncoder);
+GFX_API GfxResult gfxCommandEncoderBeginRenderPass(GfxCommandEncoder commandEncoder,
     const GfxTextureView* colorAttachments, uint32_t colorAttachmentCount,
     const GfxColor* clearColors,
     GfxTextureView depthStencilAttachment,
     float depthClearValue, uint32_t stencilClearValue,
     GfxRenderPassEncoder* outRenderPass);
-GfxResult gfxCommandEncoderBeginComputePass(GfxCommandEncoder commandEncoder, const char* label, GfxComputePassEncoder* outComputePass);
-void gfxCommandEncoderCopyBufferToBuffer(GfxCommandEncoder commandEncoder,
+GFX_API GfxResult gfxCommandEncoderBeginComputePass(GfxCommandEncoder commandEncoder, const char* label, GfxComputePassEncoder* outComputePass);
+GFX_API void gfxCommandEncoderCopyBufferToBuffer(GfxCommandEncoder commandEncoder,
     GfxBuffer source, uint64_t sourceOffset,
     GfxBuffer destination, uint64_t destinationOffset,
     uint64_t size);
-void gfxCommandEncoderCopyBufferToTexture(GfxCommandEncoder commandEncoder,
+GFX_API void gfxCommandEncoderCopyBufferToTexture(GfxCommandEncoder commandEncoder,
     GfxBuffer source, uint64_t sourceOffset, uint32_t bytesPerRow,
     GfxTexture destination, const GfxOrigin3D* origin,
     const GfxExtent3D* extent, uint32_t mipLevel);
-void gfxCommandEncoderCopyTextureToBuffer(GfxCommandEncoder commandEncoder,
+GFX_API void gfxCommandEncoderCopyTextureToBuffer(GfxCommandEncoder commandEncoder,
     GfxTexture source, const GfxOrigin3D* origin, uint32_t mipLevel,
     GfxBuffer destination, uint64_t destinationOffset, uint32_t bytesPerRow,
     const GfxExtent3D* extent);
-void gfxCommandEncoderFinish(GfxCommandEncoder commandEncoder);
+GFX_API void gfxCommandEncoderFinish(GfxCommandEncoder commandEncoder);
 
 // RenderPassEncoder functions
-void gfxRenderPassEncoderDestroy(GfxRenderPassEncoder renderPassEncoder);
-void gfxRenderPassEncoderSetPipeline(GfxRenderPassEncoder renderPassEncoder, GfxRenderPipeline pipeline);
-void gfxRenderPassEncoderSetBindGroup(GfxRenderPassEncoder renderPassEncoder, uint32_t index, GfxBindGroup bindGroup);
-void gfxRenderPassEncoderSetVertexBuffer(GfxRenderPassEncoder renderPassEncoder, uint32_t slot, GfxBuffer buffer, uint64_t offset, uint64_t size);
-void gfxRenderPassEncoderSetIndexBuffer(GfxRenderPassEncoder renderPassEncoder, GfxBuffer buffer, GfxIndexFormat format, uint64_t offset, uint64_t size);
-void gfxRenderPassEncoderDraw(GfxRenderPassEncoder renderPassEncoder, uint32_t vertexCount, uint32_t instanceCount, uint32_t firstVertex, uint32_t firstInstance);
-void gfxRenderPassEncoderDrawIndexed(GfxRenderPassEncoder renderPassEncoder, uint32_t indexCount, uint32_t instanceCount, uint32_t firstIndex, int32_t baseVertex, uint32_t firstInstance);
-void gfxRenderPassEncoderEnd(GfxRenderPassEncoder renderPassEncoder);
+GFX_API void gfxRenderPassEncoderDestroy(GfxRenderPassEncoder renderPassEncoder);
+GFX_API void gfxRenderPassEncoderSetPipeline(GfxRenderPassEncoder renderPassEncoder, GfxRenderPipeline pipeline);
+GFX_API void gfxRenderPassEncoderSetBindGroup(GfxRenderPassEncoder renderPassEncoder, uint32_t index, GfxBindGroup bindGroup);
+GFX_API void gfxRenderPassEncoderSetVertexBuffer(GfxRenderPassEncoder renderPassEncoder, uint32_t slot, GfxBuffer buffer, uint64_t offset, uint64_t size);
+GFX_API void gfxRenderPassEncoderSetIndexBuffer(GfxRenderPassEncoder renderPassEncoder, GfxBuffer buffer, GfxIndexFormat format, uint64_t offset, uint64_t size);
+GFX_API void gfxRenderPassEncoderDraw(GfxRenderPassEncoder renderPassEncoder, uint32_t vertexCount, uint32_t instanceCount, uint32_t firstVertex, uint32_t firstInstance);
+GFX_API void gfxRenderPassEncoderDrawIndexed(GfxRenderPassEncoder renderPassEncoder, uint32_t indexCount, uint32_t instanceCount, uint32_t firstIndex, int32_t baseVertex, uint32_t firstInstance);
+GFX_API void gfxRenderPassEncoderEnd(GfxRenderPassEncoder renderPassEncoder);
 
 // ComputePassEncoder functions
-void gfxComputePassEncoderDestroy(GfxComputePassEncoder computePassEncoder);
-void gfxComputePassEncoderSetPipeline(GfxComputePassEncoder computePassEncoder, GfxComputePipeline pipeline);
-void gfxComputePassEncoderSetBindGroup(GfxComputePassEncoder computePassEncoder, uint32_t index, GfxBindGroup bindGroup);
-void gfxComputePassEncoderDispatchWorkgroups(GfxComputePassEncoder computePassEncoder, uint32_t workgroupCountX, uint32_t workgroupCountY, uint32_t workgroupCountZ);
-void gfxComputePassEncoderEnd(GfxComputePassEncoder computePassEncoder);
+GFX_API void gfxComputePassEncoderDestroy(GfxComputePassEncoder computePassEncoder);
+GFX_API void gfxComputePassEncoderSetPipeline(GfxComputePassEncoder computePassEncoder, GfxComputePipeline pipeline);
+GFX_API void gfxComputePassEncoderSetBindGroup(GfxComputePassEncoder computePassEncoder, uint32_t index, GfxBindGroup bindGroup);
+GFX_API void gfxComputePassEncoderDispatchWorkgroups(GfxComputePassEncoder computePassEncoder, uint32_t workgroupCountX, uint32_t workgroupCountY, uint32_t workgroupCountZ);
+GFX_API void gfxComputePassEncoderEnd(GfxComputePassEncoder computePassEncoder);
 
 // Fence functions
-void gfxFenceDestroy(GfxFence fence);
-GfxResult gfxFenceGetStatus(GfxFence fence, bool* isSignaled);
-GfxResult gfxFenceWait(GfxFence fence, uint64_t timeoutNs);
-void gfxFenceReset(GfxFence fence);
+GFX_API void gfxFenceDestroy(GfxFence fence);
+GFX_API GfxResult gfxFenceGetStatus(GfxFence fence, bool* isSignaled);
+GFX_API GfxResult gfxFenceWait(GfxFence fence, uint64_t timeoutNs);
+GFX_API void gfxFenceReset(GfxFence fence);
 
 // Semaphore functions
-void gfxSemaphoreDestroy(GfxSemaphore semaphore);
-GfxSemaphoreType gfxSemaphoreGetType(GfxSemaphore semaphore);
-GfxResult gfxSemaphoreSignal(GfxSemaphore semaphore, uint64_t value);
-GfxResult gfxSemaphoreWait(GfxSemaphore semaphore, uint64_t value, uint64_t timeoutNs);
-uint64_t gfxSemaphoreGetValue(GfxSemaphore semaphore);
+GFX_API void gfxSemaphoreDestroy(GfxSemaphore semaphore);
+GFX_API GfxSemaphoreType gfxSemaphoreGetType(GfxSemaphore semaphore);
+GFX_API GfxResult gfxSemaphoreSignal(GfxSemaphore semaphore, uint64_t value);
+GFX_API GfxResult gfxSemaphoreWait(GfxSemaphore semaphore, uint64_t value, uint64_t timeoutNs);
+GFX_API uint64_t gfxSemaphoreGetValue(GfxSemaphore semaphore);
 
 // ============================================================================
 // Utility Functions
