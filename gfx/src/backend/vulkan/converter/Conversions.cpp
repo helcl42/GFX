e1acba7c0319b27ca7ac10bf8a92a467b20a59e0
helcl42 <lubos.helcl@gmail.com>
2026-01-29 23:23:20 +0100

    Fix formats stensic component

--- gfx/src/backend/vulkan/converter/Conversions.cpp
+++ gfx/src/backend/vulkan/converter/Conversions.cpp
@@ -388,7 +388,7 @@ VkPresentModeKHR gfxPresentModeToVkPresentMode(GfxPresentMode mode)
 
 bool hasStencilComponent(VkFormat format)
 {
-    return format == VK_FORMAT_D24_UNORM_S8_UINT || format == VK_FORMAT_D32_SFLOAT_S8_UINT;
+    return format == VK_FORMAT_D24_UNORM_S8_UINT || format == VK_FORMAT_D32_SFLOAT_S8_UINT || format == VK_FORMAT_S8_UINT;
 }
 
 VkImageAspectFlags getImageAspectMask(VkFormat format)
